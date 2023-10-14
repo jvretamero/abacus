3c3baf11e976774776522aa5b3ea37b06f72e4ef
João Vitor Retamero <joaovretamero@gmail.com>
2023-10-14 13:19:55 -0300

    Addition operation

--- lib/domain/calculator.dart
+++ lib/domain/calculator.dart
@@ -3,14 +3,22 @@ import 'package:abacus/domain/localization_provider.dart';
 class Calculator {
   final StringBuffer _buffer = StringBuffer();
   late LocalizationProvider _localizationProvider;
+  double _leftOperand = 0;
 
   Calculator(LocalizationProvider localizationProvider) {
     _localizationProvider = localizationProvider;
   }
 
-  double get currentNumber => _localizationProvider.parse(_currentNumberText);
+  double get _currentNumber {
+    if (_buffer.isEmpty) return 0;
+    return _localizationProvider.parse(_currentNumberText);
+  }
 
   String get _currentNumberText => _buffer.toString();
+
+  double calculate() {
+    return _leftOperand + _currentNumber;
+  }
 
   void onNumber(int number) {
     _buffer.write(number);
@@ -21,5 +29,10 @@ class Calculator {
     if (numberText.contains(_localizationProvider.decimalSeparator)) return;
 
     _buffer.write(_localizationProvider.decimalSeparator);
+  }
+
+  void onAdd() {
+    _leftOperand = _currentNumber;
+    _buffer.clear();
   }
 }
--- test/domain/calculator_test.dart
+++ test/domain/calculator_test.dart
@@ -15,14 +15,14 @@ void main() {
     test('single digit number', () {
       calculator.onNumber(1);
 
-      expect(calculator.currentNumber, 1);
+      expect(calculator.calculate(), 1);
     });
 
     test('multiple digits', () {
       calculator.onNumber(2);
       calculator.onNumber(3);
 
-      expect(calculator.currentNumber, 23);
+      expect(calculator.calculate(), 23);
     });
   });
 
@@ -32,14 +32,14 @@ void main() {
       calculator.onDecimal();
       calculator.onNumber(1);
 
-      expect(calculator.currentNumber, 1.1);
+      expect(calculator.calculate(), 1.1);
     });
 
     test('parse number with only integer digits', () {
       calculator.onNumber(1);
       calculator.onDecimal();
 
-      expect(calculator.currentNumber, 1);
+      expect(calculator.calculate(), 1);
     });
 
     test('accept only one decimal separator', () {
@@ -47,14 +47,31 @@ void main() {
       calculator.onDecimal();
       calculator.onDecimal();
 
-      expect(calculator.currentNumber, 1);
+      expect(calculator.calculate(), 1);
     });
 
     test('accept number with only decimal digits', () {
       calculator.onDecimal();
       calculator.onNumber(1);
 
-      expect(calculator.currentNumber, 0.1);
+      expect(calculator.calculate(), 0.1);
+    });
+  });
+
+  group('Addition operation', () {
+    test('add two numbers', () {
+      calculator.onNumber(1);
+      calculator.onAdd();
+      calculator.onNumber(1);
+
+      expect(calculator.calculate(), 2);
+    });
+
+    test('return left operand when right operand is not provided', () {
+      calculator.onNumber(1);
+      calculator.onAdd();
+
+      expect(calculator.calculate(), 1);
     });
   });
 }
